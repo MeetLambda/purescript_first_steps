62d86f04e039ec14b3034bd67f69b1f820eb10a3
fsolaroli <fabio.solaroli@gmail.com>
2020-03-21 10:52:08 +0100

    Prove

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,16 @@
 module Main where
 
---import Prelude -- core functions
+-- import Prelude -- core functions
+import Control.Applicative (pure)
 import Control.Apply ((*>))
-import Control.Bind (discard, (>>=))
+import Control.Bind (bind, discard, (>>=))
 import Data.Semigroup ((<>))
 import Data.Show (class Show, show)
 import Data.Unit (Unit)
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Semiring ((+), (*))
+import Data.Maybe (Maybe (..))
 
 -------------------------------
 
@@ -58,6 +61,7 @@ showPersonADT :: PersonADT -> String
 showPersonADT (Person r) = r.name <> " IS " <> show r.age <> " YEARS OLD"
 
 -------------------------------
+
 mainWithApplicativeDo :: Effect Unit
 mainWithApplicativeDo = do
   log ("- applicative 'do'")
@@ -79,9 +83,50 @@ mainWithApplySecond =
   log (showPersonADT newPersonADT) *>
   log (showPersonaLikeRecord { name: "Bombo", age: 6 , super: "Super"})
 
+-------------------------------
+
+foo :: Int -> Int -> Int
+foo x y = x * y + y
+test :: Int
+test = 10 `foo` 20
+test1 :: Int
+test1 = foo 10 20
+fooBy2 :: Int -> Int
+fooBy2 = (_ `foo` 2)
+
+-------------------------------
+
+f :: Int -> String
+f = 
+  case _ of
+    0 -> "None"
+    1 -> "One"
+    _ -> "Some"
+
+-------------------------------
+
+maybeSum :: Maybe Number -> Maybe Number -> Maybe Number
+maybeSum a b = do
+  n <- a
+  m <- b
+  let result = n + m
+  pure result
+
+------------------------------- 
+
+mainTest :: Effect Unit
+mainTest = do
+  log (show test)
+  log (show test1)
+  log (show (fooBy2 50))
+  log (f 1)
+  log (show (maybeSum (Just 5.0) (Just 5.0)))
+
+-------------------------------
 
 main :: Effect Unit
 main = do
   mainWithApplicativeDo
   mainWithBind
   mainWithApplySecond
+  mainTest
