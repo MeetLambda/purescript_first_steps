c181a7ffb97da496f9b8aafc9d18051a3f74209c
Giuliano.Santandrea <gsantandrea@imolainformatica.it>
2020-03-28 12:57:00 +0100

    contributions of 28-03-2020 meetup

--- src/Main.purs
+++ src/Main.purs
@@ -6,20 +6,28 @@ import Control.Apply ((*>))
 import Control.Applicative (pure)
 import Control.Bind (bind, discard, (>>=))
 import Control.MonadZero (guard)
+import Control.Semigroupoid ((<<<))
+import Data.Array as Data.Array
 import Data.Array ((..))
 import Data.Boolean (otherwise)
+import Data.Char.Unicode as Data.Char.Unicode
 import Data.Either (Either(..))
 import Data.Eq ((==), class Eq)
+import Data.HeytingAlgebra ((&&))
+import Data.String.Common (null)
+import Data.Foldable as Data.Foldable
 import Data.Function (flip, ($))
+import Data.Functor as Data.Functor
 import Data.Maybe (Maybe(..))
-import Data.Ord ((<), (>), class Ord)
-import Data.Ring ((-))
+import Data.Ord ((<), (>), (>=), (<=), class Ord)
+import Data.Ring (class Ring, (-), negate)
 import Data.Semigroup ((<>))
 import Data.Semiring ((+), (*))
 import Data.Show (class Show, show)
+import Data.String.Common as Data.String.Common
+--import Data.String.CodeUnits (fromCharArray, toCharArray)
 import Data.Tuple (Tuple, Tuple(..))
 import Data.Unit (Unit)
-import Data.Functor (map)
 import Effect (Effect)
 import Effect.Console (log)
 import Data.Array as Array
@@ -168,10 +176,10 @@ maybeSum' a b =
       in pure result
 
 
--- show' xs = "[" <> joinWith ", " (map show xs) <> "]"
+-- show' xs = "[" <> joinWith ", " (Data.Functor.map show xs) <> "]"
 -- show'' xs = "[" <> arrayAsList <> "]"
 --   where
---     arrayAsList = joinWith ", " (map show xs)
+--     arrayAsList = joinWith ", " (Data.Functor.map show xs)
 
 -- ============================================
 --  Instance Chains
@@ -208,8 +216,8 @@ class Stream stream element | stream -> element where
   uncons :: stream -> Maybe { head :: element, tail :: stream }
 
 genericTail :: forall stream element. Stream stream element => stream -> Maybe stream
-genericTail xs = map (\a -> a.tail) (uncons xs)
-genericTail' xs = map _.tail (uncons xs)
+genericTail xs = Data.Functor.map (\a -> a.tail) (uncons xs)
+genericTail' xs = Data.Functor.map _.tail (uncons xs)
 
 factors :: Int -> Array (Tuple Int Int)
 factors n = do
@@ -217,7 +225,129 @@ factors n = do
   b <- 1 .. a
   guard $ a * b == n
   pure $ Tuple a b
- 
+
+-- ===================================================
+--  Finding Success and Failure
+-- ===================================================
+
+-- absVal :: (Num a, Ord a) => a -> a
+-- absVal :: forall a. Ring a => Ord a => a -> a
+absVal :: Number -> Number
+absVal x = if (x < 0.0) then (negate x) else x
+
+validateUsernamePassword :: String -> String -> String
+validateUsernamePassword username password =
+  if null username
+  then (
+      if null password
+      then "Empty username and password"
+      else "Empty username"
+  ) else (
+    if null password
+    then "Empty password"
+    else "Okay"
+  )
+
+sort :: String -> String
+sort = String.fromCharArray <<< Data.Array.sort <<< String.toCharArray 
+
+reverse :: String -> String
+reverse = String.fromCharArray <<< Data.Array.reverse <<< String.toCharArray 
+
+isAllChar :: (Char -> Boolean) -> String -> Boolean
+isAllChar f w = Data.Foldable.all f (String.toCharArray w)
+
+isWord :: String -> Maybe String
+isWord word =
+  case (null word) of
+    true  -> Nothing
+    false -> case (isAllChar (Data.Char.Unicode.isAlpha) word) of
+      false -> Nothing
+      true  -> Just word
+
+isAnagram :: String -> String -> Boolean
+isAnagram word1 word2 = (sort word1) == (sort word2)
+
+checkAnagram :: String -> String -> String
+checkAnagram word1 word2 = 
+  case (isWord word1) of
+    Nothing -> "The first word is invalid."
+    Just word1 -> 
+      case (isWord word2) of
+        Nothing -> "The second word is invalid."
+        Just word2 ->
+          case (isAnagram word1 word2) of
+            false -> "These words are not anagrams."
+            true  -> "These words are anagrams."
+
+substituteChar :: Char -> Char 
+substituteChar c = 
+  case c of 
+    'e'        -> '3'
+    'a'        -> '@'
+    'i'        -> '1'
+    'o'        -> '0'
+    otherwise  ->  c
+
+translateWord :: String -> String
+translateWord w = String.fromCharArray $ Data.Functor.map substituteChar (String.toCharArray w)
+
+checkPasswordLength :: Int -> Int -> String -> Maybe String
+checkPasswordLength min max s =
+  case ((min <= String.length s) && (String.length s <= max)) of
+    true  -> Just s
+    false -> Nothing
+
+checkPasswordLength' :: Int -> Int -> String -> Either String String
+checkPasswordLength' min max s =
+  if (String.length s <= min)
+  then Left "Value too short"
+  else  if (max <= String.length s)
+        then Left "Value too long"
+        else Right s
+
+requireAlphaNum :: String -> Maybe String
+requireAlphaNum word =
+  case (isAllChar Data.Char.Unicode.isAlphaNum word) of
+    true -> Just word
+    false -> Nothing
+
+requireAlphaNum' :: String -> Either String String
+requireAlphaNum' word =
+  case (isAllChar Data.Char.Unicode.isAlphaNum word) of
+    true -> Right word
+    false -> Left "Non alphanum char present"
+
+cleanWhitespace :: String -> Maybe String 
+cleanWhitespace word = 
+  let tw = Data.String.Common.trim word
+   in if (null tw) then Nothing else Just tw
+
+cleanWhitespace' :: String -> Either String String
+cleanWhitespace' word = 
+  let tw = Data.String.Common.trim word
+   in if (null tw) then Left "Empty value" else Right tw
+
+cleanWhiteSpaceFabio :: String -> Maybe String
+cleanWhiteSpaceFabio = isWord <<< Data.String.Common.trim
+
+validatePassword :: String -> Maybe String
+validatePassword password = 
+  cleanWhitespace password
+  >>= requireAlphaNum
+  >>= (checkPasswordLength 8 30)
+
+validatePassword' :: String -> Either String String
+validatePassword' password = 
+  cleanWhitespace' password
+  >>= requireAlphaNum'
+  >>= (checkPasswordLength' 8 30)
+
+-- (>>=) :: Monad m => m a -> (a -> m b) -> m b
+-- bind :: forall a b. m a -> (a -> m b) -> m b
+
+-- ===================================================
+
 main :: Effect Unit
 main = do
   -- mainWithApplicativeDo
@@ -228,8 +358,28 @@ main = do
   -- log (g (Just true) (Right true))
   -- log (show (maybeSum (Just 1.0) (Just 3.0)))
   -- log (show (maybeSum (Nothing) (Just 3.0)))
-  log $ f' "ciao" -- hello
-  log $ f' true -- true
-  log $ f' MysteryItem
-  log $ f'' MysteryItem -- Invalid
-
+  -- log $ f' "ciao" -- hello
+  -- log $ f' true -- true
+  -- log $ f' MysteryItem
+  -- log $ f'' MysteryItem -- Invalid
+  -- log $ validateUsernamePassword "joe" "clipperz"
+  -- log $ validateUsernamePassword "joe" ""
+  -- log $ show (checkAnagram "Filippo" "Giulio")
+  -- log $ show (checkAnagram "cat" "tac")
+  -- log $ translateWord "letsspeak"
+  log $ show $ checkPasswordLength 8 30 "ciao"
+  log $ show (checkPasswordLength 8 30 "letsspeak")
+  log $ show $ validatePassword' "arrivederci1234"
+  log $ show $ validatePassword' "@rrivederci1234"
+  log $ show $ validatePassword' "1234"
+  -- log $ show (requireAlphaNum "ciao54321")
+  -- log $ show (requireAlphaNum "ciao. ciao.")
+  -- log $ show (requireAlphaNum "letsspeak")
+  -- log $ show (requireAlphaNum $ translateWord "letsspeak")
+  -- log $ show (cleanWhiteSpaceFabio "  prova  ")
+  -- log $ show (cleanWhiteSpaceFabio "       ")
+  -- log $ show (isWord "ciao")
+  -- log $ show (isWord "")
+  -- log $ show (isWord "pippo")
+  -- log $ show (isWord "pipp0")
+  
